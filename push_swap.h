216a48d12540c688a4b375dcf92d598905847170
textualbois <wivna360@hotmail.com>
2024-05-30 17:28:10 +0200

    added funciton declarations

--- push_swap.h
+++ push_swap.h
@@ -14,4 +14,34 @@ typedef struct s_stacknode{
     t_stacknode *prev;
 }	t_stacknode;
 
+//a_2_b_helpers.c
+void        get_a_node_pos(int *pos, int *len, t_stacknode *node, t_stacknode *head);
+void        get_b_node_insert(int *pos, int *len, int value, t_stacknode *head);
+void        get_pos_alt(int *pos, int val, t_stacknode *head);
+
+//arr_utils.c
+size_t      ft_arrlen(char **arr);
+void        *clear_arr(char **res_arr);
+void        rm_el(char **arr, int index, int arr_len);
+
+//clear.c
+t_stacknode *clear_list(t_stacknode *head);
+
+//init.c
+t_stacknode *init_stack_a(int *inputs);
+
+//operations_1.c
+t_stacknode *create_new_node(int value);
+void	    make_circular(t_stacknode *head, t_stacknode *last);
+void	    add_after(t_stacknode *current, t_stacknode *new);
+void	    add_before(t_stacknode *current, t_stacknode *new);
+
+//oporations_2.c
+void	    insert_after(t_stacknode *current, t_stacknode *new);
+void	    insert_before(t_stacknode *current, t_stacknode *new);
+
+//wild_math.c
+int         int_min(int a, int b);
+int         int_max(int a, int b);
+
 #endif
