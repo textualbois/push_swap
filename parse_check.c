9cdaad7ca843eacf6cd691d88e7691e72d6e4e3d
textualbois <wivna360@hotmail.com>
2024-05-31 13:00:41 +0200

    switch form arr_len to argc + type mismatch fixes

--- parse_check.c
+++ parse_check.c
@@ -18,13 +18,11 @@ int	is_int(char *str)
 
 int *parse_check(int argc, char **argv)
 {
-	int i;
 	int *res;
 
 	pre_parse(&argc, &argv);
 	if (argv == NULL)
 		return (werror("Error\n", 2));
-	i = 0;
 	res = transform(argc, argv);
 	clear_arr(argv);
 	return (res);
@@ -32,26 +30,26 @@ int *parse_check(int argc, char **argv)
 
 void	pre_parse(int *argc, char ***argv)
 {
-	if (argc > 2)
+	if (*argc > 2)
 		*argv = rm_el(*argv, 0, *argc);
 	if (*argc == 2)
 	{
-		*argv == ft_split(*argv[1], ' ');
+		*argv = ft_split(*argv[1], ' ');
 		*argc = ft_arrlen(*argv);
 	}
 	else
 		*argv = NULL;
 }
 
-int		*transform(int argc, char **arr)
+int	*transform(int argc, char **arr)
 {
 	int	*res;
 	int	i;
 
-	res = malloc((ft_arrlen(arr) + 1) * sizeof(int));
+	res = malloc((argc + 1) * sizeof(int));
 	if (res == NULL)
 		return (NULL);
-	res[0] = ft_arrlen(arr);
+	res[0] = argc;
 	i = 0;
 	while (arr[i] != NULL)
 	{
