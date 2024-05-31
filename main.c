74584ae519dd751d19d409f9db63a98b287f26cd
textualbois <wivna360@hotmail.com>
2024-05-31 12:28:14 +0200

    added parse tester temporarily

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 int main (int argc, char **argv)
 {
     int *input;
+    int i = 0;
 
     input = parse_check(argc, argv);
     if (input == NULL)
     {
-        free(input);
-        bad_input();
+        free(input); // dont need to free as it is freed in parse_check
+        bad_input(); // error message is also in parse_check
     }
     else
     {
-        exec_push_swap(input);
+        while (i <= input[0])
+        {
+            printf("%i\n", input[i]);
+            i++;
+        }
+        //exec_push_swap(input);
     }
     
 }
