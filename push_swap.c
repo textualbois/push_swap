b75390c4bfdb4337946b6efb96c0087dbf62c8ee
textualbois <wivna360@hotmail.com>
2024-06-04 20:45:45 +0200

    error statement for failed a_stack init

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,14 @@ void    execute_push_swap(int *inputs)
     t_stacknode *b_head;
 
     a_head = init_stack_a(inputs);
-    werror("Error\n", 2);
     if (a_head != NULL)
     {
         b_head = NULL;
         while (a_head != NULL)
             move_2_stack_b(&a_head, &b_head);
     }
+    else
+        werror("Error\n", 2);
 }
 
 void    move_2_stack_b(t_stacknode **a_head, t_stacknode **b_head)
