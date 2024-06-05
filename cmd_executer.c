3bb95791f39e013772f2c0b193b76f15a2bea529
textualbois <wivna360@hotmail.com>
2024-06-05 17:15:12 +0200

    fixed wrong str lens

--- cmd_executer.c
+++ cmd_executer.c
@@ -7,7 +7,7 @@ void	move_both_up(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos)
 		rr(a_head, b_head);
 		pos.a--;
 		pos.b--;
-		write(1, "rr\n", 4);
+		write(1, "rr\n", 3);
 	}
 	while (pos.a != 0)
 	{
@@ -36,13 +36,13 @@ void	move_both_down(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos, t
 	{
 		rev_rotate(a_head);
 		pos.a++;
-		write(1, "rra\n", 3);
+		write(1, "rra\n", 4);
 	}
 	while (pos.b != len.b)
 	{
 		rev_rotate(b_head);
 		pos.b++;
-		write(1, "rrb\n", 3);
+		write(1, "rrb\n", 4);
 	}
 	rev_rr(a_head, b_head);
 	write(1, "rrr\n", 4);
@@ -60,7 +60,7 @@ void	move_a_up_b_down(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos,
 	{
 		rev_rotate(b_head);
 		pos.b++;
-		write(1, "rrb\n", 3);
+		write(1, "rrb\n", 4);
 	}
 }
 
@@ -70,7 +70,7 @@ void	move_a_down_b_up(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos,
 	{
 		rev_rotate(a_head);
 		pos.a++;
-		write(1, "rra\n", 3);
+		write(1, "rra\n", 4);
 	}
 	while (pos.b != 0)
 	{
