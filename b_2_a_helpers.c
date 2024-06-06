e87769e607e19940a31b1cdd117450f208634eaa
Ivan Semin <isemin@3-C-9.42heilbronn.de>
2024-06-06 17:59:25 +0200

    fixed reset for b stack

--- b_2_a_helpers.c
+++ b_2_a_helpers.c
@@ -8,9 +8,10 @@ static void	get_largest_pos(int *to_top, int *to_bottom, t_stacknode *head)
 
 	current = head;
 	max = current->value;
+	*to_top = 0;
 	count = 0;
 	current = current->next;
-	while (current->value <= max && current != head)
+	while (current->value < max && current != head)
 	{
 		count++;
 		current = current->next;
@@ -33,19 +34,19 @@ void	reset_order(t_stacknode **head)
 	int	to_top;
 	int	to_bottom;
 
-	ft_putstr_fd("\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\nreseting stack b order\n", 1);
-	ft_putstr_fd("stack was :\n", 1);
-	print_stack(*head);
+	// ft_putstr_fd("\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\nreseting stack b order\n", 1);
+	// ft_putstr_fd("stack was :\n", 1);
+	// print_stack(*head);
 	get_largest_pos(&to_top, &to_bottom, *head);
 	// write(1, "to_top = ", 9);
 	// ft_putnbr_fd(to_top, 1);
 	// write(1, "\n", 2);
 	// write(1, "to_bottom = ", 12);
 	// ft_putnbr_fd(to_bottom, 1);
-	// write(1, "\n", 2);
+	// write(1, "\n\n", 2);
 	if (to_top <= to_bottom)
 	{
-		while(to_top != 0)
+		while (to_top != 0)
 		{
 			rev_rotate(head);
 			to_top--;
@@ -61,7 +62,7 @@ void	reset_order(t_stacknode **head)
 			write(1, "rb\n", 3);
 		}
 	}
-	ft_putstr_fd("\nstack became :\n", 1);
-	print_stack(*head);
-	ft_putstr_fd("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n", 1);
+// 	ft_putstr_fd("\nstack became :\n", 1);
+// 	print_stack(*head);
+// 	ft_putstr_fd("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n", 1);
 }
