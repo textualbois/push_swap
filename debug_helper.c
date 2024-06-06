408e099fec9574f6ab8310e23d4ad83caea20c5d
Ivan Semin <isemin@3-C-9.42heilbronn.de>
2024-06-06 15:32:41 +0200

    debug too that prints a stack

---
+++ debug_helper.c
@@ -0,0 +1,24 @@
+#include "push_swap.h"
+
+void	print_stack(t_stacknode *head)
+{
+	t_stacknode	*temp;
+
+	if (head == NULL)
+		write(1, "stack NULL\n", ft_strlen("stack NULL\n"));
+	else
+	{
+		temp = head;
+		write(1, "stack start\n", ft_strlen("stack start\n"));
+		ft_putnbr_fd(temp->value, 1);
+		temp = temp->next;
+		while (temp != head)
+		{
+			write(1, " ", 1);
+			ft_putnbr_fd(temp->value, 1);
+			temp = temp->next;
+		}
+		write(1, "\n", 1);
+		write(1, "stack end\n", ft_strlen("stack end\n"));
+	}
+}
