a691ebe16bdc1bb67de57d0009842ff452ad168e
textualbois <wivna360@hotmail.com>
2024-06-16 18:34:31 +0200

    added fix for when you need to move stack a down, but not stack b

--- cmd_executer.c
+++ cmd_executer.c
@@ -8,72 +8,44 @@ void	move_both_up(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos)
 		pos.a--;
 		pos.b--;
 		write(1, "rr\n", 3);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.a != 0)
 	{
 		rotate(a_head);
 		pos.a--;
 		write(1, "ra\n", 3);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.b != 0)
 	{
 		rotate(b_head);
 		pos.b--;
 		write(1, "rb\n", 3);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 }
 
 void	move_both_down(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos, t_twoints len)
 {
-	while (pos.a != len.a && pos.b < len.b - 1)
+	while (pos.a != len.a && pos.b != len.b - 1)
 	{
 		rev_rr(a_head, b_head);
 		pos.a++;
 		pos.b++;
 		write(1, "rrr\n", 4);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.a != len.a)
 	{
 		rev_rotate(a_head);
 		pos.a++;
 		write(1, "rra\n", 4);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.b < len.b - 1)
 	{
 		rev_rotate(b_head);
 		pos.b++;
 		write(1, "rrb\n", 4);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	rev_rr(a_head, b_head);
 	write(1, "rrr\n", 4);
-	// ft_putstr_fd("A = ", 1);
-	// print_stack(*a_head);
-	// ft_putstr_fd("B = ", 1);
-	// print_stack(*b_head);
 }
 
 void	move_a_up_b_down(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos, t_twoints len)
@@ -83,20 +55,12 @@ void	move_a_up_b_down(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos,
 		rotate(a_head);
 		pos.a--;
 		write(1, "ra\n", 3);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.b < len.b)
 	{
 		rev_rotate(b_head);
 		pos.b++;
 		write(1, "rrb\n", 4);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 }
 
@@ -107,19 +71,11 @@ void	move_a_down_b_up(t_stacknode **a_head, t_stacknode **b_head, t_twoints pos,
 		rev_rotate(a_head);
 		pos.a++;
 		write(1, "rra\n", 4);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 	while (pos.b != 0)
 	{
 		rotate(b_head);
 		pos.b--;
 		write(1, "rb\n", 3);
-		// ft_putstr_fd("A = ", 1);
-		// print_stack(*a_head);
-		// ft_putstr_fd("B = ", 1);
-		// print_stack(*b_head);
 	}
 }
