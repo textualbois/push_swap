81ad5da64c9aaa39940a2045cef77ea8419e8cd8
textualbois <wivna360@hotmail.com>
2024-06-17 17:22:45 +0200

    no operations when input empty

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 18:38:37 by isemin            #+#    #+#             */
-/*   Updated: 2024/06/16 18:38:38 by isemin           ###   ########.fr       */
+/*   Updated: 2024/06/17 17:19:42 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ int	main(int argc, char **argv)
 {
 	int	*input;
 
-	input = parse_check(argc, argv);
-	if (input != NULL && no_duplicates(input))
+	if (argc > 1)
 	{
-		execute_push_swap(input);
-		free(input);
+		input = parse_check(argc, argv);
+		if (input != NULL && no_duplicates(input))
+		{
+			execute_push_swap(input);
+			free(input);
+		}
+		else
+			werror("Error\n", 2);
 	}
-	else
-		werror("Error\n", 2);
 }
